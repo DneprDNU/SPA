56225c88f8103d6ea5de0219ff8ea4a40ee4527d
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2015-01-23 20:20:47 +0200

    Finish files for resource entity

--- app/scripts/controllers/admin/resource/resource.js
+++ app/scripts/controllers/admin/resource/resource.js
@@ -10,7 +10,26 @@
 angular.module('dnuApp')
   .controller('ResourceAdminEditCtrl', function ($scope, $rootScope, $location, $routeParams, $upload, restResource, restCategories, restSubjects) {
     $scope.save = function() {
-      restResource.update($scope.resource);
+      if ($scope.resource.image[0] !== undefined) {
+        var imageFile = $scope.resource.image[0];
+        $scope.upload = $upload.upload({
+          url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id,
+          method: 'PUT',
+          data: {resource: $scope.resource},
+          file: imageFile
+        });
+      }
+
+      if ($scope.resource.file[0] !== undefined) {
+        var resourceFile = $scope.resource.file[0];
+        $scope.upload = $upload.upload({
+          url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id,
+          method: 'PUT',
+          data: {resource: $scope.resource},
+          file: resourceFile
+        });
+      }
+
       $location.path('/admin/resources');
     };
 
@@ -27,75 +46,31 @@ angular.module('dnuApp')
     $scope.createNewResource = function () {
       $location.path('/admin/resource');
     };
-
-    $scope.$watch('resource.image', function() {
-      if ($scope.resource.image) {
-        for (var i = 0; i < $scope.resource.image.length; i++) {
-          var file = $scope.resource.image[i];
-          console.log(file);
-          $scope.upload = $upload.upload({
-            url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id, // upload.php script, node.js route, or servlet url
-            method: 'PUT',
-            //headers: {'Authorization': 'xxx'}, // only for html5
-            //withCredentials: true,
-            data: {resource: $scope.resource},
-            file: file // single file or a list of files. list is only for html5
-            //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-            //fileFormDataName: myFile, // file formData name ('Content-Disposition'), server side request form name
-            // could be a list of names for multiple files (html5). Default is 'file'
-            //formDataAppender: function(formData, key, val){}  // customize how data is added to the formData.
-            // See #40#issuecomment-28612000 for sample code
-
-          }).progress(function(evt) {
-            console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
-          }).success(function(data, status, headers, config) {
-            // file is uploaded successfully
-            console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-          });
-          //.error(...)
-          //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
-          //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to
-          //the underlying XMLHttpRequest
-        }
-      }
-    });
-    $scope.$watch('resource.file', function() {
-      if ($scope.resource.file) {
-        for (var i = 0; i < $scope.resource.file.length; i++) {
-          var file = $scope.resource.file[i];
-          console.log(file);
-          $scope.upload = $upload.upload({
-            url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id, // upload.php script, node.js route, or servlet url
-            method: 'PUT',
-            //headers: {'Authorization': 'xxx'}, // only for html5
-            //withCredentials: true,
-            data: {resource: $scope.resource},
-            file: file // single file or a list of files. list is only for html5
-            //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-            //fileFormDataName: myFile, // file formData name ('Content-Disposition'), server side request form name
-            // could be a list of names for multiple files (html5). Default is 'file'
-            //formDataAppender: function(formData, key, val){}  // customize how data is added to the formData.
-            // See #40#issuecomment-28612000 for sample code
-
-          }).progress(function(evt) {
-            console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
-          }).success(function(data, status, headers, config) {
-            // file is uploaded successfully
-            console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-          });
-          //.error(...)
-          //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
-          //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to
-          //the underlying XMLHttpRequest
-        }
-      }
-    });
   });
 
 angular.module('dnuApp')
   .controller('ResourceAdminCreateCtrl', function ($scope, $location, $routeParams, restResources, restCategories, restSubjects) {
     $scope.save = function() {
-      restResources.create($scope.resource);
+      if ($scope.resource.image[0] !== undefined) {
+        var imageFile = $scope.resource.image[0];
+        $scope.upload = $upload.upload({
+          url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id,
+          method: 'POST',
+          data: {resource: $scope.resource},
+          file: imageFile
+        });
+      }
+
+      if ($scope.resource.file[0] !== undefined) {
+        var resourceFile = $scope.resource.file[0];
+        $scope.upload = $upload.upload({
+          url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id,
+          method: 'POST',
+          data: {resource: $scope.resource},
+          file: resourceFile
+        });
+      }
+
       $location.path('/admin/resources');
     };
 
@@ -105,68 +80,4 @@ angular.module('dnuApp')
     $scope.cancel = function () {
       $location.path('/admin/resources');
     };
-
-    $scope.$watch('resource.image', function() {
-      if ($scope.resource.image) {
-        for (var i = 0; i < $scope.resource.image.length; i++) {
-          var file = $scope.resource.image[i];
-          console.log(file);
-          $scope.upload = $upload.upload({
-            url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id, // upload.php script, node.js route, or servlet url
-            method: 'PUT',
-            //headers: {'Authorization': 'xxx'}, // only for html5
-            //withCredentials: true,
-            data: {resource: $scope.resource},
-            file: file // single file or a list of files. list is only for html5
-            //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-            //fileFormDataName: myFile, // file formData name ('Content-Disposition'), server side request form name
-            // could be a list of names for multiple files (html5). Default is 'file'
-            //formDataAppender: function(formData, key, val){}  // customize how data is added to the formData.
-            // See #40#issuecomment-28612000 for sample code
-
-          }).progress(function(evt) {
-            console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
-          }).success(function(data, status, headers, config) {
-            // file is uploaded successfully
-            console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-          });
-          //.error(...)
-          //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
-          //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to
-          //the underlying XMLHttpRequest
-        }
-      }
-    });
-    $scope.$watch('resource.file', function() {
-      if ($scope.resource.file) {
-        for (var i = 0; i < $scope.resource.file.length; i++) {
-          var file = $scope.resource.file[i];
-          console.log(file);
-          $scope.upload = $upload.upload({
-            url: 'http://' + $rootScope.serviceIp + ':8080/filestorage/rest/resource/' + $routeParams.id, // upload.php script, node.js route, or servlet url
-            method: 'PUT',
-            //headers: {'Authorization': 'xxx'}, // only for html5
-            //withCredentials: true,
-            data: {resource: $scope.resource},
-            file: file // single file or a list of files. list is only for html5
-            //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
-            //fileFormDataName: myFile, // file formData name ('Content-Disposition'), server side request form name
-            // could be a list of names for multiple files (html5). Default is 'file'
-            //formDataAppender: function(formData, key, val){}  // customize how data is added to the formData.
-            // See #40#issuecomment-28612000 for sample code
-
-          }).progress(function(evt) {
-            console.log('progress: ' + parseInt(100.0 * evt.loaded / evt.total) + '% file :'+ evt.config.file.name);
-          }).success(function(data, status, headers, config) {
-            // file is uploaded successfully
-            console.log('file ' + config.file.name + 'is uploaded successfully. Response: ' + data);
-          });
-          //.error(...)
-          //.then(success, error, progress); // returns a promise that does NOT have progress/abort/xhr functions
-          //.xhr(function(xhr){xhr.upload.addEventListener(...)}) // access or attach event listeners to
-          //the underlying XMLHttpRequest
-        }
-      }
-    });
-
   });
