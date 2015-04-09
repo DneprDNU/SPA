fee0075a6a22c2d8d757168ad48f1b82ebfa9ea6
Artyom Miroshnik <artyom.miroshnik@propeople.com.ua>
2015-04-09 22:08:55 +0300

    Fix admin menu links

--- app/scripts/controllers/mainMenu.js
+++ app/scripts/controllers/mainMenu.js
@@ -4,4 +4,5 @@ angular.module('dnuApp')
   .controller('MainMenuCtrl', function ($scope, $rootScope) {
     $scope.isAdmin = $rootScope.hasRole('ROLE_ADMIN') || $rootScope.hasRole('ROLE_SUPERADMIN');
     $scope.loggedIn = $rootScope.user !== undefined;
+    $scope.logout = $rootScope.logout;
   });
